daa33f0e0d445df799c6189cb02dcd1003e72952
Aditi <aditi1004arya@gmail.com>
2024-09-01 20:48:44 +0530

    Initial commit

---
+++ frontend/src/App.js
@@ -0,0 +1,102 @@
+import './App.css';
+import Footer from './Componets/Footerr/Footer';
+import Home from './Componets/Home/Home';
+import Navbar from './Componets/Navbar/Navbar';
+import { Routes,Route } from 'react-router-dom';
+import Register from './Componets/auth/Register';
+import Intern from "./Componets/Internships/Intern"
+import JobAvl from "./Componets/Job/JobAvl";
+import JobDetail from './Componets/Job/JobDetail';
+import InternDeatil from "./Componets/Internships/InternDeatil"
+import { useDispatch, useSelector } from 'react-redux';
+import { login,logout,selectUser } from "./Feature/Userslice"
+import { useEffect } from 'react';
+import { auth } from './firebase/firebase';
+import Profile from './profile/Profile';
+import AdminLogin from './Admin/AdminLogin';
+import Adminpanel from './Admin/Adminpanel';
+import ViewAllApplication from "./Admin/ViewAllApplication"
+import Postinternships from './Admin/Postinternships';
+import DeatilApplication from './Applications/DeatilApplication';
+import UserApplicatiom from './profile/UserApplicatiom';
+import UserapplicationDetail from "./Applications/DeatilApplicationUser";
+// import  LoginButton  from "./Components/auth/login"
+// import  LogoutButton  from "./Components/auth/logout"
+import Avatar from 'react-avatar';
+import ChatBot from 'react-chatbotify'
+import { useState } from 'react'
+import reactLogo from './Assets/react logo.png'
+import viteLogo from './Assets/vite logo.png'
+
+function App() {
+  const [count, setCount] = useState(0)
+  const user=useSelector(selectUser);
+  const dispatch=useDispatch();
+  useEffect(() => {
+    auth.onAuthStateChanged((authUser)=>{
+      if(authUser){
+        dispatch(login({
+  
+          uid:authUser.uid,
+          photo:authUser.photoURL,
+          name:authUser.displayName,
+          email:authUser.email,
+          phoneNumber:authUser.phoneNumber
+        }))
+      }
+        else{
+          dispatch(logout())
+        }
+    })
+    },[dispatch] );
+  return (
+    <div className="App">
+<Navbar/>
+
+<Avatar skypeId="sitebase" size="200" />
+<Avatar color={Avatar.getRandomColor('sitebase', ['red', 'green', 'blue'])} name="Aditi Arya" title="First Avator"/>
+{/* <button className="btn3"><LoginButton/> </button>
+<button className="btn4"> <LogoutButton/> </button> */}
+<ChatBot/>
+      {/* <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p> */}
+<Routes>
+  <Route path='/' element={<Home/>}/>
+<Route path='/register' element={<Register/>}/>
+<Route path='/internship' element={<Intern/>}/>
+<Route path='/Jobs' element={<JobAvl/>}/>
+<Route path='/profile' element={<Profile/>}/>
+<Route path='/detailjob' element={<JobDetail/>}/>
+<Route path='/detailInternship' element={<InternDeatil/>}/>
+<Route path='/detailApplication' element={<DeatilApplication/>}/>
+<Route path='/adminLogin' element={<AdminLogin/>}/>
+<Route path='/adminepanel' element={<Adminpanel/>}/>
+<Route path='/postInternship' element={<Postinternships/>}/>
+<Route path='/applications' element={<ViewAllApplication/>}/>
+<Route path='/UserapplicationDetail' element={< UserapplicationDetail/>}/>
+<Route path='/userapplication' element={<UserApplicatiom/>}/>
+</Routes>
+<Footer/>
+    </div>
+  );
+}
+
+export default App;

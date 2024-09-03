43aa43b01e4e5473a1b8f222327de3f9ded5b38b
Aditi <aditi1004arya@gmail.com>
2024-09-03 14:07:48 +0530

    Fourth commit
    Changed nested css problem

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -62,27 +62,7 @@ function App() {
 <Avatar color={Avatar.getRandomColor('sitebase', ['red', 'green', 'blue'])} name="Aditi Arya" title="First Avator"/> */}
 {/* <button className="btn3"><LoginButton/> </button>
 // <button className="btn4"> <LogoutButton/> </button> */}
- {/* <ChatBot flow={flow}/> */}
-      {/* <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p> */}
+
 <Routes>
   <Route path='/' element={<Home/>}/>
 <Route path='/register' element={<Register/>}/>
